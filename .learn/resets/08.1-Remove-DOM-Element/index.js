40d8fef51ee97c1f095d41dcfdcfc042655a183f
Martín <martinjcaro@gmail.com>
2025-03-07 08:34:16 +0000

    first commit

--- .learn/resets/08.1-Remove-DOM-Element/index.js
+++ .learn/resets/08.1-Remove-DOM-Element/index.js
@@ -1,5 +1,4 @@
 // If you check the HTML, you will find that the second <li> has the id=secondElement
 // You can use that to your advantage like a CSS selector
-let element = document.querySelector("#secondElement");
+
 // Your code here
-element.parentNode.removeChild(element);
--- exercises/08.2-Remove-DOM-Element/index.js
+++ exercises/08.2-Remove-DOM-Element/index.js
@@ -1,1 +1,4 @@
 // Your code here
+let list = document.querySelector("#parentLi");
+let secondLi = list.childNodes[3];
+list.removeChild(secondLi)
